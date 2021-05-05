6a19314bdafe8b64226253d50001c5cd394cf2ba
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-05-05 12:40:46 +0200

    fixed peak

--- goetc/simulation.py
+++ goetc/simulation.py
@@ -208,8 +208,8 @@ class Simulation:
         # peak
         #self.peak = np.floor((es / (2. * math.pi * sig2) + (n_sky + n_dark) / px_aper) / gain + bias)
         self.peak = np.floor((es / (2. * math.pi * sig2) + n_dark / self.eff_pixels) / gain + bias)
-        scale = scipy.special.erf(1/np.sqrt(8))**2
-        self.integrated_peak = np.floor((es * scale / sig2 + n_dark / self.eff_pixels) / gain + bias)
+        scale = scipy.special.erf(1/np.sqrt(8*np.sqrt(sig2)))**2
+        self.integrated_peak = np.floor((es * scale + n_dark / self.eff_pixels) / gain + bias)
         print(sky.seeing, self.peak, self.integrated_peak, self.integrated_peak / self.peak)
 
     def solid_angle_of_aperture(self, aper_radius: u.arcsec, seeing: Angle):
